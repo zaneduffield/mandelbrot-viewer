removed cython, attempted to fix overflow issue for series approximation, selection rectangle can be any dimensions now

mandelbrot.py
@@ -7,9 +7,9 @@ from numba import jit, jitclass, njit, prange
 from numba import int32, float32, float64, complex128, int64
 from bigfloat import BigFloat, Context, setcontext
 from complex_bf import ComplexBf
-from iterate import iterate, BREAKOUT_R_2
-from pertubations import mandelbrot_pertubation, get_new_ref
 
+BREAKOUT_R_2 = 20
+from pertubations import mandelbrot_pertubation, get_new_ref
 
 @njit(fastmath=True, parallel=True, nogil=True)
 def mandelbrot(t_left, b_right, height, width, iters):
@@ -40,7 +40,7 @@ def mandelbrot(t_left, b_right, height, width, iters):
 
 class Mandelbrot:
     def __init__(self, width: int, height: int, t_left: ComplexBf, b_right: ComplexBf, iterations: int,
-                 multiprocessing: bool, cython: bool, pertubations: bool, num_series_terms, num_probes):
+                 multiprocessing: bool, pertubations: bool, num_series_terms, num_probes):
         self.w = width
         self.h = height
         self.corners_stack = []
@@ -48,7 +48,6 @@ class Mandelbrot:
         self.init_corners = (t_left, b_right)
         self._set_corners(t_left=t_left, b_right=b_right)
         self.iterations = iterations
-        self.cython = cython
         self.multiprocessing = multiprocessing
 
         self.pertubations = pertubations
@@ -102,14 +101,12 @@ class Mandelbrot:
     def get_width(self):
         return float(self.b_right.real - self.t_left.real)
 
-    def getPixels(self):
+    def get_pixels(self):
         if self.pertubations:
             self.pixels, self.best_ref = mandelbrot_pertubation(self.t_left, self.b_right, self.h, self.w, self.iterations, self.num_probes, self.num_series_terms, self.best_ref)
             self.pixels = np.array(self.pixels, dtype=np.int32)
-        elif not self.cython:
+        else:
             self.pixels = mandelbrot(complex(self.t_left), complex(self.b_right), self.h, self.w, self.iterations)
-        # else:
-        #     self.pixels = iterate(self.b_left, self.t_right, self.h, self.w, self.iterations, self.multiprocessing, self.pertubations)
 
 
 def translate(value, leftMin, leftMax, rightMin, rightMax):

pertubations.py
@@ -3,20 +3,19 @@ import random
 from typing import List
 
 import numpy as np
-from numba import jit, jitclass, njit, prange
+from numba import jit, jitclass, njit, prange, gdb
 from numba import int32, float32, float64, complex128, int64
 from bigfloat import BigFloat, Context, setcontext
 from complex_bf import ComplexBf
 from mandelbrot import BREAKOUT_R_2
-
 BREAKOUT_R = math.sqrt(BREAKOUT_R_2)
 ERROR_THRESH = 0.00001
 GLITCH_ITER = -1
 
-@njit
+
+# @njit
 def _iterate_series_constants(ref_hist, ref_escaped_at: int, probe_deltas_init, terms, num_terms: int):
     probe_deltas_cur = probe_deltas_init.copy()
-    num_terms = len(terms)
     terms[0][0] = 1
     for i in range(1, num_terms):
         terms[i][0] = 0
@@ -28,6 +27,8 @@ def _iterate_series_constants(ref_hist, ref_escaped_at: int, probe_deltas_init, 
             s = 0
             for k in range(j):
                 s += terms[k][i] * terms[j - k - 1][i]
+                if np.isnan(s):
+                    break
             terms[j][i + 1] = 2 * z_comp * terms[j][i] + s
 
         for j in range(len(probe_deltas_init)):
@@ -37,7 +38,10 @@ def _iterate_series_constants(ref_hist, ref_escaped_at: int, probe_deltas_init, 
             z_del_app = 0
             delta = probe_deltas_init[j]
             for k in range(num_terms):
-                z_del_app += terms[k][i + 1] * delta
+                term = terms[k][i + 1]
+                if np.isnan(term):
+                    break
+                z_del_app += term * delta
                 delta *= probe_deltas_init[j]
             if abs(probe_deltas_cur[j] - z_del_app) > ERROR_THRESH:
                 return i
@@ -76,7 +80,6 @@ def compute_series_constants(t_left: ComplexBf, b_right: ComplexBf, ref_init: Co
 
 
 MAX_GLITCH_FIX_LOOPS = 5
-NUM_SERIES_TERMS = 10
 NUM_RANDOM_REFS_DESPARATE = 15
 MAX_GLITCH_COUNT = 10
 def mandelbrot_pertubation(t_left: ComplexBf, b_right: ComplexBf, height, width, iterations, num_probes, num_series_terms, init_ref):
@@ -104,7 +107,7 @@ def mandelbrot_pertubation(t_left: ComplexBf, b_right: ComplexBf, height, width,
         print("iterating reference")
         ref_hist, ref_hist_abs, ref_escaped_at = iterate_ref(ref, iterations)
         print("computing series constants...")
-        terms, iter_accurate = compute_series_constants(t_left, b_right, ref, ref_hist, ref_escaped_at, iterations, NUM_SERIES_TERMS, num_probes)
+        terms, iter_accurate = compute_series_constants(t_left, b_right, ref, ref_hist, ref_escaped_at, iterations, num_series_terms, num_probes)
 
         print(f"proceeding with {iter_accurate} reference iterations")
         print(f"reference broke out at {ref_escaped_at}")
@@ -293,7 +296,10 @@ class PertubationComputer:
         delta = init_delta
         out = 0
         for k in range(self.num_terms):
-            out += self.terms[k][iteration] * delta
+            term = self.terms[k][iteration]
+            if np.isnan(term):
+                break
+            out += term * delta
             delta *= init_delta
         return out
 